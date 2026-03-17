a947f5ea7666751d8225e3ad8b84ca4090a18db0
lkarp <69141474+lkarp-744@users.noreply.github.com>
2026-03-17 17:18:55 +0100

    Fix SVG attribute

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -73,7 +73,7 @@ export const BiAlignLeft = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
@@ -87,7 +87,7 @@ export const BiAlignRight = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
@@ -100,7 +100,7 @@ export const BiAlignJustify = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
@@ -113,7 +113,7 @@ export const BiAlignMiddle = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
@@ -127,7 +127,7 @@ export const IoIosListBox = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 512 512"
     height="1em"
     width="1em"
@@ -141,7 +141,7 @@ export const IoMdListBox = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 512 512"
     height="1em"
     width="1em"
@@ -155,7 +155,7 @@ export const MdOutlineExpand = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
@@ -170,7 +170,7 @@ export const MdOutlineCollapse = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
@@ -185,7 +185,7 @@ export const BiDotsVerticalRounded = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     aria-hidden="true"
     focusable="false"
